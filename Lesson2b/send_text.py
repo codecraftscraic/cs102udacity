2936cee0d490aa2888cd126337ba06e800f0de77
Jenny Jensen <jlcjensen@gmail.com>
2015-05-13 09:17:03 -0400

    Removing auth tokens

--- Lesson2b/send_text.py
+++ Lesson2b/send_text.py
@@ -1,8 +1,7 @@
 from twilio.rest import TwilioRestClient
  
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "AC61fa951e63aadcb413f98852b2c90b2e"
-auth_token  = "556f7472a58f45d8f59e648ffdf55a8d"
+
 client = TwilioRestClient(account_sid, auth_token)
  
 message = client.messages.create(body="Jenny please?! I love you <3",
